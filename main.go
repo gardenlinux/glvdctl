bcb06c44910960fb73e366f48cae810d1d1696ea
Florian Wilhelm <florian.wilhelm02@sap.com>
2025-06-26 16:47:38 +0200

    show cves in table

--- main.go
+++ main.go
@@ -23,31 +23,42 @@ type sourcePackageCve struct {
 
 func main() {
 	app := tview.NewApplication()
-	list := tview.NewList()
+	flex := tview.NewFlex()
+	table := tview.NewTable()
 
-	url := "https://glvd.ingress.glvd.gardnlinux.shoot.canary.k8s-hana.ondemand.com/v1/cves/1592.10"
+	dropdown := tview.NewDropDown().
+		SetLabel("Garden Linux version: ").
+		SetOptions([]string{"1877.0", "1592.10", "1592.9"}, func(text string, index int) {
+			table.Clear()
+			url := "https://glvd.ingress.glvd.gardnlinux.shoot.canary.k8s-hana.ondemand.com/v1/cves/" + text
+			req, err := http.NewRequest("GET", url, nil)
+			if err != nil {
+				log.Fatal(err)
+			}
+			res, err := http.DefaultClient.Do(req)
+			if err != nil {
+				log.Fatal(err)
+			}
+			defer res.Body.Close()
+			body, _ := io.ReadAll(res.Body)
+			var results []sourcePackageCve
+			err = json.Unmarshal(body, &results)
+			if err != nil {
+				log.Fatal(err)
+			}
 
-	req, _ := http.NewRequest("GET", url, nil)
+			for i, cve := range results {
+				table.SetCellSimple(i, 0, cve.CveId)
+				table.SetCellSimple(i, 1, fmt.Sprintf("%.1f", cve.BaseScore))
+				table.SetCellSimple(i, 2, cve.SourcePackageName)
+				table.SetCellSimple(i, 3, cve.SourcePackageVersion)
+			}
+		})
 
-	res, _ := http.DefaultClient.Do(req)
+	flex.AddItem(dropdown, 0, 1, true)
+	flex.AddItem(table, 0, 1, false)
 
-	defer res.Body.Close()
-	body, _ := io.ReadAll(res.Body)
-
-	var results []sourcePackageCve
-	err := json.Unmarshal(body, &results)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	var foo rune = 'a'
-	for _, y := range results {
-		fmt.Println(y)
-		list.AddItem(y.CveId, y.SourcePackageName, foo, nil)
-		foo++
-	}
-
-	if err := app.SetRoot(list, true).EnableMouse(true).Run(); err != nil {
+	if err := app.SetRoot(flex, true).EnableMouse(true).Run(); err != nil {
 		panic(err)
 	}
 }
